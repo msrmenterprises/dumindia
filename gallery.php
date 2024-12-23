fa7c46144d954a70acc22b836faf2d2f261c9a43
Manish <manish@Manishs-Air.attlocal.net>
2024-12-23 09:31:12 -0600

    Gallewry Pag e

--- gallery.php
+++ gallery.php
@@ -18,14 +18,15 @@
 	
 
 	<h3 style="text-align: left; padding-bottom: 10px;">DUM 2024 Inaugural Conference Session 14_NOVEMBER 2024 (THURSDAY)</h3>
+	<a data-flickr-embed="true" data-header="true" data-footer="true" href="https://www.flickr.com/photos/indiasmartgridforum/albums/72177720322042883" title="DUM_2024 | Inauguration of Conference and Exhibition | 14 Nov 2024">
+	<img src="https://live.staticflickr.com/65535/54149472072_4707f54d02_z.jpg" width="640" height="480" target="_blank" alt="DUM_2024 | Inauguration of Conference and Exhibition | 14 Nov 2024"/></a>
+	<script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>
+	
+	<h3 style="text-align: left; padding-bottom: 10px;">DUM 2024 Exhibition Inaugural 14_November 2024 (Thursday)</h3>
 	<a data-flickr-embed="true" data-header="true" data-footer="true" href="https://www.flickr.com/photos/indiasmartgridforum/albums/72177720322052811" title="DUM_2024 | Exhibition Inaugural | 14 Nov 2024">
 		<img src="https://live.staticflickr.com/65535/54151870057_66bd3bd56b_z.jpg" width="640" height="480" target="_blank" alt="DUM_2024 | Exhibition Inaugural | 14 Nov 2024"/></a>
 		<script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>
 
-	<h3 style="text-align: left; padding-bottom: 10px;">DUM 2024 Exhibition Inaugural 14_November 2024 (Thursday)</h3>
-	<a data-flickr-embed="true" data-header="true" data-footer="true" href="https://www.flickr.com/photos/indiasmartgridforum/albums/72177720322042883" title="DUM_2024 | Inauguration of Conference and Exhibition | 14 Nov 2024">
-	<img src="https://live.staticflickr.com/65535/54149472072_4707f54d02_z.jpg" width="640" height="480" target="_blank" alt="DUM_2024 | Inauguration of Conference and Exhibition | 14 Nov 2024"/></a>
-	<script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>
 
 	<h3 style="text-align: left; padding-bottom: 10px;">DUM 2024 Session 1: Special Plenary Session – Sustainability of DISCOMs (Part 1) 14_November 2024 (Thursday)_11:30~13:00</h3>
 	<a data-flickr-embed="true" data-header="true" data-footer="true" href="https://www.flickr.com/photos/indiasmartgridforum/albums/72177720322029922" title="DUM_2024 | Session1:  Special Plenary Session - Sustainability of DISCOMs (Part 1) | 11:30 ~ 13:00 IST | 14 Nov 2024">

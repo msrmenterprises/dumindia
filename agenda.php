93c8683b5380b98def0230edbe343cd3ba801a56
Manish <manish@Manishs-Air.attlocal.net>
2025-04-09 22:14:34 -0500

    IWT-182

--- agenda.php
+++ agenda.php
@@ -7,18 +7,17 @@
 
 <div class="row dum_container">
 	<div class="k_cheading">
-		<h1><span class="k_greencolor">DUM 2024 AGENDA</span> </h1>
+		<h1><span class="k_greencolor">DUM 2025 AGENDA</span> </h1>
 		<img src="images/kborder_bottom.png" alt="">
 	</div>
 	<br>
 	<!--<div class="k_register k_contact_us">-->
-	<iframe src="dumpdf/Dum24.pdf" style="width:90%;height:500px;"></iframe><br><br>
-		<!--<div class="col-sm-12" style="text-align: center;">
-			<iframe src="http://dumindia.in/dumpdf/Dum 2024 Agenda Abridged.pdf" style="width:100%;height:700px;"></iframe><br><br>
+	<!-- <iframe src="dumpdf/Dum24.pdf" style="width:90%;height:500px;"></iframe><br><br> -->
+		<div class="col-sm-12" style="text-align: center;">
 			
-			<!--<p style="font-size:17px; text-align:center;"><b>Agenda of DUM 2024 will be updated closer to the Program on Website. To Participate, please write to Organizers at <br/> <a href="mailto:Dum@Indiasmartgrid.org">Dum@Indiasmartgrid.org</a>
+			<p style="font-size:17px; text-align:center;"><b>Agenda of DUM 2024 will be updated closer to the Program on Website. To Participate, please write to Organizers at <br/> <a href="mailto:Dum@Indiasmartgrid.org">Dum@Indiasmartgrid.org</a>
 		</p>
-		</div>-->
+		</div>
 	<!--</div>
 	<br>
 	<br> 

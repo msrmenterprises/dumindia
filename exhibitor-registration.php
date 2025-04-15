9732fd3fc68df82907153f216d29c281c6a8f6df
Manish <manish@Manishs-Air.attlocal.net>
2025-04-14 21:24:19 -0500

    IWT-208 Exhibitor Rehistratoon Page

---
+++ exhibitor-registration.php
@@ -0,0 +1,64 @@
+<?php include "include/header.php"; ?>
+<style>
+.k_confirmed_als li a img {
+    border: 1px solid #aeb4b2;
+    max-height: 80px;
+   
+    max-width: 100%;
+    object-fit: contain;
+    border: 1px solid #aeb4b2 !important;
+    display: block;
+    margin: 0 auto 7px;
+}
+.k_confirmed_als h5 {text-transform: UPPERCASE;}
+</style>
+<!-- key objective starts here -->
+<div class="row k_inbanner">
+	<img src="images/baner1.jpg" alt="">
+</div>
+<div class="row dum_container key_div">
+	<div class="k_cheading">
+			<h1>EXHIBITION BOOTH AT <span class="k_greencolor">DUM</span></h1>
+			<img src="images/kborder_bottom.png" alt="">
+	</div>
+
+    <p style="text-align:center;">Utilities and select Technology Providers have the option to exhibit their products and solutions in the booths which will give a unique experience of interacting with visitors from around the globe and explaining about their offerings.<br/>
+	<b>Exhibition Booths are available in following options​:<br/>
+	<table style="width: 80%; margin: 20px auto; border-collapse: collapse; text-align: left;">
+        <thead>
+            <tr style="background-color: #8cc63e; color: #fff;">
+                <th style="padding: 10px; border: 1px solid #ddd;">Exhibition Booth Option</th>
+                <th style="padding: 10px; border: 1px solid #ddd;">Exhibition Space (SQM)</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td style="padding: 10px; border: 1px solid #ddd;">Platinum</td>
+                <td style="padding: 10px; border: 1px solid #ddd;">18 SQM</td>
+            </tr>
+            <tr>
+                <td style="padding: 10px; border: 1px solid #ddd;">Gold</td>
+                <td style="padding: 10px; border: 1px solid #ddd;">9 SQM</td>
+            </tr>
+            <tr>
+                <td style="padding: 10px; border: 1px solid #ddd;">Silver</td>
+                <td style="padding: 10px; border: 1px solid #ddd;">Shared Space</td>
+            </tr>
+            <tr>
+                <td style="padding: 10px; border: 1px solid #ddd;">Start-Up</td>
+                <td style="padding: 10px; border: 1px solid #ddd;">Shared Space</td>
+            </tr>
+        </tbody>
+    </table>
+    
+</div>
+	
+		
+
+
+
+
+
+<!-- key objective ends here -->
+
+<?php include "include/footer.php"; ?>

f0400148e4528fdddd4eaf0e950b41e9bee84605
Manish <manish@Manishs-Air.attlocal.net>
2025-04-16 20:01:40 -0500

    IWT-207 Home page change

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 
 
 
-	<ul>
+	<ul style="position: relative;top: 229px;left: 227px;">
 
 		<li><a href="images/invite.ics" class="k_green" style="cursor: pointer;" class="pull-left"><i class="fa fa-calendar" aria-hidden="true"></i>06 - 07 November 2025</a></li>
 

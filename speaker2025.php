66ad537b171b1cb00645269d459d69d1702e7db4
Manish <manish@Mac.attlocal.net>
2025-04-27 21:37:03 -0500

    IWT-220

--- speaker2025.php
+++ speaker2025.php
@@ -889,18 +889,18 @@ His team also focuses on energy transition, energy modeling, providing specializ
 						<h1 class="key_head" style="font-size: 25px; text-align: left;    text-transform: capitalize;">
 							Reena Suri<br/> Executive Director <br/> India Smart Grid Forum
 						</h1>
-						<p>Reena Suri, Executive Director of the India Smart Grid Forum (ISGF) since 2013, brings
-over 22 years of rich experience in the energy sector. She has played a key role in
-various ISGF projects, including the Electrification Plan for Public Transportation,
-Blockchain for Electric Utilities, Energy Storage Roadmap for India, Smart Grid
-Roadmap, and developing smart grid and EV infrastructure plans for the SAARC
-Region.</p>
-<p> Reena also led the World Bank EV Project in Kolkata. As the founder-editor of
-the Smart Grid Bulletin, Reena has been a voice for smart grid advancements since
-2013. She is passionate about increasing gender diversity in the energy sector, leading
-initiatives to improve networking and mentoring programs for women, raise
-awareness and technology skills among women, and attract women to technical
-education by showcasing inspiring role models.</p>
+							<p>Reena Suri, Executive Director of the India Smart Grid Forum (ISGF) since 2013, brings
+							over 22 years of rich experience in the energy sector. She has played a key role in
+							various ISGF projects, including the Electrification Plan for Public Transportation,
+							Blockchain for Electric Utilities, Energy Storage Roadmap for India, Smart Grid
+							Roadmap, and developing smart grid and EV infrastructure plans for the SAARC
+							Region.</p>
+							<p> Reena also led the World Bank EV Project in Kolkata. As the founder-editor of
+							the Smart Grid Bulletin, Reena has been a voice for smart grid advancements since
+							2013. She is passionate about increasing gender diversity in the energy sector, leading
+							initiatives to improve networking and mentoring programs for women, raise
+							awareness and technology skills among women, and attract women to technical
+							education by showcasing inspiring role models.</p>
 
 					</div>
 				</a>
@@ -921,45 +921,45 @@ education by showcasing inspiring role models.</p>
 						<h1 class="key_head" style="font-size: 25px; text-align: left;    text-transform: capitalize;">
 						Reji Kumar Pilla<br/> President, India Smart Grid Forum (ISGF) <br> Chairman, Global Smart Energy Federation (GSEF)
 						</h1>
-						<p>Reji is the President of India Smart Grid Forum (<a href="https://www.indiasmartgrid.org" target="_blank">www.indiasmartgrid.org</a>) since its
-inception in 2011 and is also the current Chairman of the Global Smart Energy
-Federation (GSEF). He is an internationally renowned expert with over four decades
-of experience in the electricity sector in diverse functions covering the entire value
-chain and across continents. He is spearheading a mission to leverage technology
-to transform the electric grids and provide clean and reliable electricity to every
-citizen 24x7 at affordable cost. For decades he has been actively advocating for the
-“Right to Electricity” that would ensure lifeline supply of electricity to all which is now
-becoming a reality in India and several other developing countries; and also
-advocating for the “Right to Sleep” to provide climate-controlled sleeping pods in
-low-income communities.</p>
-<p> Reji has played pivotal role in several groundbreaking
-initiatives related to renewable energy, electric grid modernization, electric
-mobility, energy storage and digitalization of utilities across continents during the
-past two decades. His current areas of research and work spans: Grid Integrated
-Vehicles, District Cooling Systems, Electric Cooking, Integration of Utilities – electricity +
-water + city gas distribution; Grid Interactive Buildings and Campuses; Artificial
-Intelligence, Robotics, Blockchain, Web 3.0 and Metaverse Applications for Utilities; Urban
-Air Mobility (UAM) Systems and the Future of Transportation; and Interconnection of
-Regional Grids in Asia - ASEAN, SAARC and GCC grids.</p>
-<p> In November 2016, Reji was
-unanimously elected as Chairman of Global Smart Grid Federation, now renamed as
-Global Smart Energy Federation (GSEF), a global umbrella organization of smart
-energy associations and utilities from around the globe headquartered in Washington
-DC, USA that was established by the US Department of Energy in 2010. Reji is also an
-advisor to Arab Renewable Energy Commission (AREC) since 2017; and work closely
-with the Association of Power Utilities in Africa (APUA), International Smart Grid
-Action Network (ISGAN) and LATAM Smart Grid Forum.</p>
-<p> As an entrepreneur, Reji
-has built successful enterprises in India and overseas and exited them. Presently he
-mentors several technology startups around the globe. He has studied Engineering,
-Finance, Management and Law; worked with NTPC and IBM and has been a senior
-consultant with ADB, World Bank and USAID. A recognized thought leader in
-renewable energy, smart grid technologies, smart cities and electric mobility, Reji is a
-popular key-note speaker at international symposiums and conferences. He has also
-contributed to several articles, books, research reports and white papers on variety of
-topics.</p>
+									<p>Reji is the President of India Smart Grid Forum (<a href="https://www.indiasmartgrid.org" target="_blank">www.indiasmartgrid.org</a>) since its
+							inception in 2011 and is also the current Chairman of the Global Smart Energy
+							Federation (GSEF). He is an internationally renowned expert with over four decades
+							of experience in the electricity sector in diverse functions covering the entire value
+							chain and across continents. He is spearheading a mission to leverage technology
+							to transform the electric grids and provide clean and reliable electricity to every
+							citizen 24x7 at affordable cost. For decades he has been actively advocating for the
+							“Right to Electricity” that would ensure lifeline supply of electricity to all which is now
+							becoming a reality in India and several other developing countries; and also
+							advocating for the “Right to Sleep” to provide climate-controlled sleeping pods in
+							low-income communities.</p>
+							<p> Reji has played pivotal role in several groundbreaking
+							initiatives related to renewable energy, electric grid modernization, electric
+							mobility, energy storage and digitalization of utilities across continents during the
+							past two decades. His current areas of research and work spans: Grid Integrated
+							Vehicles, District Cooling Systems, Electric Cooking, Integration of Utilities – electricity +
+							water + city gas distribution; Grid Interactive Buildings and Campuses; Artificial
+							Intelligence, Robotics, Blockchain, Web 3.0 and Metaverse Applications for Utilities; Urban
+							Air Mobility (UAM) Systems and the Future of Transportation; and Interconnection of
+							Regional Grids in Asia - ASEAN, SAARC and GCC grids.</p>
+							<p> In November 2016, Reji was
+							unanimously elected as Chairman of Global Smart Grid Federation, now renamed as
+							Global Smart Energy Federation (GSEF), a global umbrella organization of smart
+							energy associations and utilities from around the globe headquartered in Washington
+							DC, USA that was established by the US Department of Energy in 2010. Reji is also an
+							advisor to Arab Renewable Energy Commission (AREC) since 2017; and work closely
+							with the Association of Power Utilities in Africa (APUA), International Smart Grid
+							Action Network (ISGAN) and LATAM Smart Grid Forum.</p>
+							<p> As an entrepreneur, Reji
+							has built successful enterprises in India and overseas and exited them. Presently he
+							mentors several technology startups around the globe. He has studied Engineering,
+							Finance, Management and Law; worked with NTPC and IBM and has been a senior
+							consultant with ADB, World Bank and USAID. A recognized thought leader in
+							renewable energy, smart grid technologies, smart cities and electric mobility, Reji is a
+							popular key-note speaker at international symposiums and conferences. He has also
+							contributed to several articles, books, research reports and white papers on variety of
+							topics.</p>
 						
-			</div>
+					</div>
 				</a>
 			</div>
 		</div>

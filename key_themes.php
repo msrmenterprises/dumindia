4f18e9638d30efaa8a735932e36bd077a95d3662
Manish <manish@Mac.attlocal.net>
2025-05-09 20:49:07 -0500

    IWT-238

--- key_themes.php
+++ key_themes.php
@@ -12,57 +12,25 @@
 
 <!--<div class="row dum_container keythemes_div">-->
 <div class="row dum_container key_div">
-	<div class="k_cheading">
-		<h1><span class="k_greencolor">KEY THEMES OF 2025</span> </h1>
-		<img src="images/kborder_bottom.png" alt="">
-	</div>
+    <div class="k_cheading">
+        <h1><span class="k_greencolor">KEY THEMES OF 2025</span> </h1>
+        <img src="images/kborder_bottom.png" alt="">
+    </div>
 
-	<p>DUM will include Plenaries and Presentations. Invited international utilities and Knowledge Partners will present on select topics. Case studies and project experiences will be discussed in the sessions. Presentations and discussions will be around below mentioned themes during DUM Conference:</b></p>
-		<br/><br/>
-		<ul>
-		<li><b>Digitalization of DISCOMs</b></li>
-		<li><b>Solarization of Agricultural Feeders Success Stories</b></li>
-		<li><b>250 Smart Metering - Progress and Challenges</b></li>
-		<li><b>RE, EV and Grid Stability </b> </li>
-		<li><b>Electric Vehicle (EV) Charging Infrastructure & Grid Integration</b></li>
-		<li><b>Health of DISCOMs</b></li>
-		<li><b>Artificial Intelligence Applications for Utilities</b></li> 
-		<li><b>Challenges of 10 million Rooftop Solar PV Systems</b></li>
-		<li><b>Experience From The Field</b></li>
-		
-		   
-        </ul>
-		<br>
-		
-		<!--
-			
-			<button class="accordion">SESSION - 5: 250 Million Smart Meters</button>
-			<div class="panel">
-			  <ul>
-				<li>Business Model – CAPEX/OPEX </li>
-<li>Integration of Smart Meters with Distribution Automation and SCADA Systems </li>
-<li>Standard Bidding Documents and Model Contract </li>
-<li>Benefits in the use of smart meter with rooftop solar and edge of the grid solutions  </li>
-<li>Prepaid or Post-paid or Mix of Both
-</li>
-						
-					</ul>
-			</div>
-			<button class="accordion">PLENARY SESSION: NEW REVENUE OPPORTUNITIES FOR DISCOMs</button>
-			<div class="panel">
-			  <ul>
-				<li>New Businesses from Unlocking Physical and Digital Assets</li>
-<li>Ancillary Market Opportunities from the Real Time Market </li>
-<li>New Business Models for EV and Storage</li>
-						
-					</ul>
-			</div>
-			
-			
-		</div>-->
-
-
-	</p>
+    <p>DUM will include Plenaries and Presentations. Invited international utilities and Knowledge Partners will present on select topics. Case studies and project experiences will be discussed in the sessions. Presentations and discussions will be around below mentioned themes during DUM Conference:</b></p>
+    <br/><br/>
+    <ul>
+        <li><b>AIDA Vision and Roadmap</b></li>
+        <li><b>Policy and Regulatory Interventions for DISCOMs Sustainability</b></li>
+        <li><b>Smart Metering and Digitalization of DISCOMs</b></li>
+        <li><b>AI Data Centers - Opportunities and Challenges</b></li>
+        <li><b>Grid Integration of RE and EV</b></li>
+        <li><b>Artificial Intelligence Applications for Utilities</b></li>
+        <li><b>Revision of Equipment Specifications to Address Rising Ambient Temperature</b></li>
+        <li><b>PPP Models for DISCOMs Sustainability</b></li>
+        <li><b>Digital Energy Grids and the Emerging Era of P2P Energy Trading</b></li>
+    </ul>
+    <br>
 </div>
 <!-- key themes ends here -->
 

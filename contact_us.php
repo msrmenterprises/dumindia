5cc316eca7815a6d85f1642741605ad583c7ea4e
Manish <manish@Mac.attlocal.net>
2025-05-20 00:20:12 -0500

    IWT-248

--- contact_us.php
+++ contact_us.php
@@ -1,71 +1,4 @@
-<?php
-/*
-date_default_timezone_set('Asia/Kolkata');
-include "init/init.php";
-include "include/header.php"; 
-require 'sendgrid_files/vendor/autoload.php'; // If you're using Composer (recommended)
-include_once('sendgrid_files/credentials.php');
 
-if($_SERVER["REQUEST_METHOD"] == "POST")
-{
-    if(isset($_POST['contactf_submit']))
-    {
-    $FROM_EMAIL = mysqli_real_escape_string($db, $_POST['email']); //preventing from sql injection
-    $FROM_EMAIL = htmlspecialchars($FROM_EMAIL); //preventing from xss attack
-     // they dont like when it comes from @gmail, prefers business emails
-    $from_name = mysqli_real_escape_string($db, $_POST['name']);  //preventing from sql injection
-    $from_name = htmlspecialchars($from_name); //preventing from xss attack
-    $TO_EMAIL = 'dum@indiasmartgrid.org';
-    // Try to be nice. Take a look at the anti spam laws. In most cases, you must
-    $txtarea=mysqli_real_escape_string($db, $_POST['txtarea']); //preventing from sql injection 
-    $malicious_tag=strpos($txtarea,"<"); //preventing from xss attack
-    $malicious_tag1=strpos($txtarea,">");
-    if($malicious_tag!== false && $malicious_tag1!== false )
-    {
-      $m="oops! Message pattern is not valid kindly recheck";
-      echo "<script>alert('$m');</script>";
-    }
-    else
-    {
-	    $time= time();
-	    $insert="INSERT INTO contact_detail (name, email, message, date_of) VALUES ('$from_name', '$FROM_EMAIL', '$txtarea', '$time')";
-	    $insert_query=mysqli_query($db, $insert);
-	    
-	    $subject = "WRITE TO US – DUM 2019 Form Filled By ".$_POST['name'];
-	    $htmlContent = '<div style="background: #eee; padding: 50px 50px; width: 100%; float: left;">
-	    <h1 style="text-align: left;font-weight: bold; margin-top: 0px; margin-bottom: 30px;">Dum Contact us Form</h1>
-	      <div style="width:500px; height:auto; line-height:21px;"><strong>Name:- </strong> '.$from_name.'</div>
-	      <div style="width:500px; height:auto; line-height:21px;"><strong>Email:- </strong> '.$FROM_EMAIL.'</div>
-	      <div style="width:90%; height:auto; line-height:21px;"><strong>Message:- </strong> '.$txtarea.'</div>
-	    </div>';
-	
-	    $email = new \SendGrid\Mail\Mail(); 
-	    $email->setFrom($FROM_EMAIL, $from_name);
-	    $email->setSubject($subject);
-	    $email->addTo($TO_EMAIL, null);
-	    $email->addContent("text/html", $htmlContent);
-	    $sendgrid = new \SendGrid($API_KEY);
-	    try 
-	    {
-	      $response = $sendgrid->send($email);
-	      $result = $response->statusCode();
-	      if($result == 202)
-	      {
-	        $m="Your Request form is successfully Submitted";
-	        echo "<script>alert('$m');</script>";
-	        // print_r($response->headers());
-	        // print $response->body() . "\n";
-	      }
-	    }
-	    catch (Exception $e) 
-	    {
-	      $m='Caught exception: '.  $e->getMessage();
-	      echo "<script>alert('$m');</script>";
-	    }
-    	}
-  }
-} */
-?>
 
 <?php
 
@@ -139,13 +72,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   border: 0px;
 }
 
-@media(max-width: 769px)
+/* @media(max-width: 769px)
 {
   .dum_contactpart1 .col-sm-6:first-child
   {
     border-right: 0px !important;
     border-bottom: 1px dotted #3333334f;
     padding-bottom: 10px;
+  }
+} */
+ @media (max-width: 767px) {
+  .dum_container .col-md-6 {
+    border-right: none !important;
+    border-bottom: 1px solid #eee;
+    margin-bottom: 30px;
   }
 }
 </style>
@@ -157,14 +97,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	<img src="images/baner1.jpg" alt="">
 </div>
 
-<div class="row dum_container">
+<!-- <div class="row dum_container">
 	<div class="k_cheading">
 		<h1><span class="k_greencolor">Event Information</span> </h1>
 		<img src="images/kborder_bottom.png" alt="">
 	</div>
 
   <div class="row dum_contact_row">
-    <!-- First Column: Event Details -->
+   
     <div class="col-sm-2">
     </div>
     <div class="col-sm-4" style="float: right;">
@@ -173,7 +113,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <a href="">Time: 10am to 6pm</a><br>
     </div>
 
-    <!-- Second Column: Contact Information -->
+    
     <div class="col-sm-4" style="float:left;">
         <h1 style="margin:0;"><b><a href="http://www.indiasmartgrid.org/" target="_blank">India Smart Grid Forum</a></b></h1>
         <a href="mailto:dum@indiasmartgrid.org">Email :- dum@indiasmartgrid.org</a><br>
@@ -196,53 +136,41 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <br>
 <br>
 	
+</div> -->
+
+<div class="row dum_container" style="margin-top: 40px;">
+  <div class="col-md-6 col-sm-12" style="border-right:1px solid #eee; margin-bottom: 30px;">
+    <div class="k_cheading">
+      <h1><span class="k_greencolor">Contact Information</span></h1>
+      <img src="images/kborder_bottom.png" alt="">
+    </div>
+    <div style="margin-top: 30px;">
+      <h2 style="margin:0 0 10px 0;"><b><a href="http://www.indiasmartgrid.org/" target="_blank">India Smart Grid Forum</a></b></h2>
+      <p><strong>Date:</strong> 06 - 07 November, 2025</p>
+      <p><strong>Location:</strong> Mumbai, Maharashtra</p>
+      <p><strong>Time:</strong> 10am to 6pm</p>
+      <p><strong>Email:</strong> <a href="mailto:dum@indiasmartgrid.org">dum@indiasmartgrid.org</a></p>
+      <p><strong>Contact No.:</strong> <a href="tel:+911141057658">+91-11-41057658</a></p>
+    </div>
+  </div>
+  <div class="col-md-6 col-sm-12">
+    <div class="k_cheading">
+      <h1><span class="k_greencolor">Write to Us</span></h1>
+      <img src="images/kborder_bottom.png" alt="">
+    </div>
+    <div class="k_register" style="margin-top: 30px;">
+      <form action="" method="post" style="text-align: center;" class="k_form">
+        <input type="text" class="form_div_k" placeholder="Your Name" required name="name">
+        <input type="email" class="form_div_k" placeholder="Your Email" required name="email">
+        <textarea style="height: 100px; width: 99%; padding-top: 7px; min-width: 99%;" class="form_div_k form_div_width" cols="30" rows="10" placeholder="Your Message" name="txtarea" required></textarea>
+        <br><br>
+        <input type="submit" class="register_button" value="Send Message" name="contactf_submit">
+      </form>
+    </div>
+  </div>
 </div>
 
 
 <?php include "include/footer.php"; ?>
 
 
-
-<!-- 
-	<div class="k_register k_contact_us">
-    <h1 class="event_info">Event Information</h1>
-   
-
-<div class="col-sm-12 dum_contact1">
-      <a href="">Date :- 14 - 15 November, 2024   </a><br>
-	  <a href="">Location:- Lucknow, Uttar Pradesh</a><br>
-      <a href="">Time: 10am to 6pm</a><br>
-    </div>
-	
-    <div class="dum_contactpart1">
-      <div class="col-sm-12">
-        <h1><b><a href="http://www.indiasmartgrid.org/" target="_blank">India Smart Grid Forum</a></b></h1>
-        <a href="">Email :- dum@indiasmartgrid.org</a><br>
-        <a href="">Contact No. :- +91-11- 41057658  </a><br>
-      </div>
-
-     
-    </div>
-
-    <br>
-    <div class="col-sm-12">
-      <hr>
-      <h1 class="event_info">Write to us</h1>
-      <form action="" method="post" style="text-align: center;" class="k_form">
-       <input type="text" class="form_div_k" placeholder="your name" required="" name="name">
-       <input type="email" class="form_div_k" placeholder="your email" required="" name="email">
-       <textarea id="" style="height: 100px; width: 99%;  padding-top: 7px; min-width: 99%;" class="form_div_k form_div_width" cols="30" rows="10" placeholder="your message" name="txtarea" required=""></textarea>
-      
-       <br><br>
-       <input type="submit" class="register_button" required="" value="send message" name="contactf_submit">
-     </form>
-   </div>
- </div>
- <br>
- <br>
- 
-</div>
-
-
-
-<?php include "include/footer.php"; ?> -->

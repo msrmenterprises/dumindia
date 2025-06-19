b6b9ad96935f84f5bc0be77c8751b172c6047906
Manish <manish@Manishs-MacBook-Air.local>
2025-06-18 22:20:38 -0400

    sdfs

--- gallery.php
+++ gallery.php
@@ -3,34 +3,34 @@
 
 </style>
 <?php include "include/header.php"; ?>
-<!-- Bootstrap 5 JS (include before </body> or at the end of header.php if possible) -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
 
 <div class="row k_inbanner">
 	<img src="images/baner1.jpg" alt="">
 </div>
 
 <div class="row dum_container key_div">
-
     <h1 class="key_head">Our Gallery</h1>
 
     <!-- Tabs Navigation -->
-    <ul class="nav nav-tabs" id="galleryTab" role="tablist">
-        <li class="nav-item" role="presentation">
-            <button class="nav-link active" id="photos-tab" data-bs-toggle="tab" data-bs-target="#photos" type="button" role="tab" aria-controls="photos" aria-selected="true">Photos</button>
-        </li>
-        <li class="nav-item" role="presentation">
-            <button class="nav-link" id="videos-tab" data-bs-toggle="tab" data-bs-target="#videos" type="button" role="tab" aria-controls="videos" aria-selected="false">Videos</button>
-        </li>
+    <ul class="nav nav-tabs" role="tablist">
+        <li class="active"><a href="#photos" role="tab" data-toggle="tab">Photos</a></li>
+        <li><a href="#videos" role="tab" data-toggle="tab">Videos</a></li>
     </ul>
 
     <!-- Tabs Content -->
-    <div class="tab-content" id="galleryTabContent" style="width:100%">
-        <div class="tab-pane fade show active" id="photos" role="tabpanel" aria-labelledby="photos-tab">
+    <div class="tab-content" style="width:100%">
+        <div class="tab-pane fade in active" id="photos">
             <p>This is the <strong>Photos</strong> tab. Add your photo gallery content here.</p>
+            <!-- Example photo -->
+            <img src="images/sample-photo.jpg" alt="Sample Photo" class="img-responsive" style="max-width:300px;">
         </div>
-        <div class="tab-pane fade" id="videos" role="tabpanel" aria-labelledby="videos-tab">
+        <div class="tab-pane fade" id="videos">
             <p>This is the <strong>Videos</strong> tab. Add your video gallery content here.</p>
+            <!-- Example video embed -->
+            <div class="embed-responsive embed-responsive-16by9" style="max-width:560px;">
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/dQw4w9WgXcQ" allowfullscreen></iframe>
+            </div>
         </div>
     </div>
 </div>
@@ -45,6 +45,11 @@
 	});
 
 </script>
+<!-- Make sure jQuery and Bootstrap 3 JS are included in your header or before </body> -->
+<!-- Example (if not already in header.php): -->
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
 
 <?php include "include/footer.php"; ?>

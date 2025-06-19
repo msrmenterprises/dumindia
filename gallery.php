8c8c1e22d8e897f6a26081ccaee93dd271af964d
Manish <manish@Manishs-MacBook-Air.local>
2025-06-18 22:24:24 -0400

    Galery update

--- gallery.php
+++ gallery.php
@@ -10,7 +10,7 @@
 </div>
 
 <div class="row dum_container key_div">
-    <h1 class="key_head">Our Gallery</h1>
+    <h1 class="key_head">DUM Photos Gallery</h1>
 
     <!-- Tabs Navigation -->
     <ul class="nav nav-tabs" role="tablist">
@@ -21,16 +21,80 @@
     <!-- Tabs Content -->
     <div class="tab-content" style="width:100%">
         <div class="tab-pane fade in active" id="photos">
-            <p>This is the <strong>Photos</strong> tab. Add your photo gallery content here.</p>
-            <!-- Example photo -->
-            <img src="images/sample-photo.jpg" alt="Sample Photo" class="img-responsive" style="max-width:300px;">
+           
+			<h2 style="text-align: center; padding-bottom: 10px;">Glimpses of DUM 2024</h2>
+	<h4 style="text-align: center; padding-bottom: 10px;">DISTRIBUTION UTILITY MEET (DUM) 2024 |  LUCKNOW, UTTAR PRADESH </h4>
+	
+
+	<h3 style="text-align: left; padding-bottom: 10px;">DUM 2024 Inaugural Conference Session 14_NOVEMBER 2024 (THURSDAY)</h3>
+	<a data-flickr-embed="true" data-header="true" data-footer="true" href="https://www.flickr.com/photos/indiasmartgridforum/albums/72177720322042883" title="DUM_2024 | Inauguration of Conference and Exhibition | 14 Nov 2024">
+	<img src="https://live.staticflickr.com/65535/54149472072_4707f54d02_z.jpg" width="640" height="480" target="_blank" alt="DUM_2024 | Inauguration of Conference and Exhibition | 14 Nov 2024"/></a>
+	<script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>
+	
+	<h3 style="text-align: left; padding-bottom: 10px;">DUM 2024 Exhibition Inaugural 14_November 2024 (Thursday)</h3>
+	<a data-flickr-embed="true" data-header="true" data-footer="true" href="https://www.flickr.com/photos/indiasmartgridforum/albums/72177720322052811" title="DUM_2024 | Exhibition Inaugural | 14 Nov 2024">
+		<img src="https://live.staticflickr.com/65535/54151870057_66bd3bd56b_z.jpg" width="640" height="480" target="_blank" alt="DUM_2024 | Exhibition Inaugural | 14 Nov 2024"/></a>
+		<script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>
+
+
+	<h3 style="text-align: left; padding-bottom: 10px;">DUM 2024 Session 1: Special Plenary Session – Sustainability of DISCOMs (Part 1) 14_November 2024 (Thursday)_11:30~13:00</h3>
+	<a data-flickr-embed="true" data-header="true" data-footer="true" href="https://www.flickr.com/photos/indiasmartgridforum/albums/72177720322029922" title="DUM_2024 | Session1:  Special Plenary Session - Sustainability of DISCOMs (Part 1) | 11:30 ~ 13:00 IST | 14 Nov 2024">
+		<img src="https://live.staticflickr.com/65535/54150894605_9eedaf10a8_z.jpg" width="640" height="480" target="_blank" alt="DUM_2024 | Session1:  Special Plenary Session - Sustainability of DISCOMs (Part 1) | 11:30 ~ 13:00 IST | 14 Nov 2024"/></a>
+		<script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>
+
+	<h3 style="text-align: left; padding-bottom: 10px;">DUM 2024 Session 2: Stories from Solarization of Agricultural Feeders and Other Notable Experiences from The Field_14 November 2024 (Thursday)_14:00~15:30</h3>
+	<a data-flickr-embed="true" data-header="true" data-footer="true" href="https://www.flickr.com/photos/indiasmartgridforum/albums/72177720322030077" title="DUM_2024 | Session 2: Stories From Solarization of Agricultural Feeders and Other Notable Experiences From The Field | 14:00 ~ 15:30 IST | 14 Nov 2024">
+		<img src="https://live.staticflickr.com/65535/54150454491_5363d7b6ba_z.jpg" width="640" height="480" target="_blank" alt="DUM_2024 | Session 2: Stories From Solarization of Agricultural Feeders and Other Notable Experiences From The Field | 14:00 ~ 15:30 IST | 14 Nov 2024"/></a>
+		<script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>
+
+	<h3 style="text-align: left; padding-bottom: 10px;">DUM 2024 Session 3: Leveraging 250 Million Smart Meters to Drive Digitalization of DISCOMs_14 November 2024 (Thursday)_15:30~17:00</h3>
+	<a data-flickr-embed="true" data-header="true" data-footer="true" href="https://www.flickr.com/photos/indiasmartgridforum/albums/72177720322048309" title="DUM_2024 | Session 3: Leveraging 250 Million Smart Meters to Drive Digitalization of DISCOMs | Session Partner: Polaris Grids | 15:30 ~ 17:00 IST | 14 Nov 2024">
+		<img src="https://live.staticflickr.com/65535/54150755378_6bbc646c9c_z.jpg" width="640" height="480" target="_blank" alt="DUM_2024 | Session 3: Leveraging 250 Million Smart Meters to Drive Digitalization of DISCOMs | Session Partner: Polaris Grids | 15:30 ~ 17:00 IST | 14 Nov 2024"/></a>
+		<script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>
+
+	<h3 style="text-align: left; padding-bottom: 10px;">DUM 2024 Session 4: Emerging Technologies for Zero Power Sector_14 November 2024 (Thursday)_17:00~18:30</h3>
+	<a data-flickr-embed="true" data-header="true" data-footer="true" href="https://www.flickr.com/photos/indiasmartgridforum/albums/72177720322029496" title="DUM_2024 | Session 4: Emerging Technologies for Net Zero Power Sector | Session Partner: L&amp;T Digital Energy Solutions, PT&amp;D | 17:00 ~ 18:30 IST | 14 Nov 2024">
+		<img src="https://live.staticflickr.com/65535/54150808844_0d97bbbd57_z.jpg" width="640" height="480" target="_blank" alt="DUM_2024 | Session 4: Emerging Technologies for Net Zero Power Sector | Session Partner: L&amp;T Digital Energy Solutions, PT&amp;D | 17:00 ~ 18:30 IST | 14 Nov 2024"/></a>
+		<script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>
+	
+
+	<h3 style="text-align: left; padding-bottom: 10px;">Cultural Program_14 November 2024 (Thursday)_19:30~23:00</h3>
+	<a data-flickr-embed="true" data-header="true" data-footer="true" href="https://www.flickr.com/photos/indiasmartgridforum/albums/72177720322029971" title="DUM_2024 | Cultural Program | 14 Nov 2024">
+		<img src="https://live.staticflickr.com/65535/54150847584_e797775d5c_z.jpg" width="640" height="480" target="_blank" alt="DUM_2024 | Cultural Program | 14 Nov 2024"/></a>
+		<script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>
+	
+
+	<h3 style="text-align: left; padding-bottom: 10px;">DUM 2024 Session 5: Special Plenary Session – Sustainability of DISCOMs (Part 2)_15 November 2024 (Friday)_10:00~11:30</h3>
+	<a data-flickr-embed="true" data-header="true" data-footer="true" href="https://www.flickr.com/photos/indiasmartgridforum/albums/72177720322031012" title="DUM_2024 | Session 5: Special Plenary Session - Sustainability of DISCOMs (Part 2) | 10:00 ~ 11:30 IST | 15 Nov 2024">
+		<img src="https://live.staticflickr.com/65535/54149697787_47bb655b51_z.jpg" width="640" height="480" target="_blank" alt="DUM_2024 | Session 5: Special Plenary Session - Sustainability of DISCOMs (Part 2) | 10:00 ~ 11:30 IST | 15 Nov 2024"/></a>
+		<script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>
+	
+
+	<h3 style="text-align: left; padding-bottom: 10px;">DUM Session 6: Electric Vehicle (EV) Charging Infrastructure and Grid Integration (Session Partner: L&T PTD Digital Energy Solutions)_15 November 2024 (Friday)_11:30~13:00</h3>
+	<a data-flickr-embed="true" data-header="true" data-footer="true" href="https://www.flickr.com/photos/indiasmartgridforum/albums/72177720322049374" title="DUM_2024 | Session 6: Electric Vehicle (EV) Charging Infrastructure and Grid Integration | 11:30 ~ 13:00 IST | 15 Nov 2024">
+		<img src="https://live.staticflickr.com/65535/54149712492_21c654aab4_z.jpg" width="640" height="480" target="_blank" alt="DUM_2024 | Session 6: Electric Vehicle (EV) Charging Infrastructure and Grid Integration | 11:30 ~ 13:00 IST | 15 Nov 2024"/></a>
+		<script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>
+
+
+	<h3 style="text-align: left; padding-bottom: 10px;">DUM 2024 Session 7: RE, EV and Grid Stability and Challenges of 10 Million Rooftop Solar PV Systems (Session Partner: GIZ)_15 November 2024 (Friday)_14:00~16:00</h3>
+	<a data-flickr-embed="true" data-header="true" data-footer="true" href="https://www.flickr.com/photos/indiasmartgridforum/albums/72177720322031912" title="DUM_2024 | Session 7: RE, EV and Grid Stability and Challenges of 10 Million Rooftop Solar PV Systems | Session Partner: GIZ | 14:00 ~ 16:00 IST | 15 Nov 2024">
+		<img src="https://live.staticflickr.com/65535/54151092945_1b41e04d66_z.jpg" width="640" height="480" target="_blank" alt="DUM_2024 | Session 7: RE, EV and Grid Stability and Challenges of 10 Million Rooftop Solar PV Systems | Session Partner: GIZ | 14:00 ~ 16:00 IST | 15 Nov 2024"/></a>
+		<script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>
+	
+
+	<h3 style="text-align: left; padding-bottom: 10px;">DUM 2024 Valedictory Session_18 November 2022 (Friday)_16:30~17:30</h3>
+	<a data-flickr-embed="true" data-header="true" data-footer="true" href="https://www.flickr.com/photos/indiasmartgridforum/albums/72177720322051275" title="DUM_2024 | Valedictory Session | 15 Nov 2024">
+		<img src="https://live.staticflickr.com/65535/54152717368_81cdc5b68e_z.jpg" width="640" height="480" target="_blank" alt="DUM_2024 | Valedictory Session | 15 Nov 2024"/></a>
+		<script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>
+	
+
+<!-- END of DUM 20204 Glimpse -->
+            
         </div>
         <div class="tab-pane fade" id="videos">
             <p>This is the <strong>Videos</strong> tab. Add your video gallery content here.</p>
             <!-- Example video embed -->
-            <div class="embed-responsive embed-responsive-16by9" style="max-width:560px;">
-                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/dQw4w9WgXcQ" allowfullscreen></iframe>
-            </div>
+            
         </div>
     </div>
 </div>
@@ -45,11 +109,5 @@
 	});
 
 </script>
-<!-- Make sure jQuery and Bootstrap 3 JS are included in your header or before </body> -->
-<!-- Example (if not already in header.php): -->
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-
 
 <?php include "include/footer.php"; ?>

dbf8837bb0464d3faf6ca87f45ed2e89aed279d2
Manish <manish@mac.mynetworksettings.com>
2025-09-22 20:57:58 -0500

    IWT-327

---
+++ industry_participation.php
@@ -0,0 +1,52 @@
+<?php include "include/header.php"; ?>
+
+<!-- key objective starts here -->
+<div class="row k_inbanner">
+	<img src="images/baner1.jpg" alt="">
+</div>
+<div class="row dum_container key_div">
+	
+	<h1 class="key_head">
+		About Distribution Utility Meet (DUM) 2019
+	</h1>
+	<p>
+ISGF has been organizing Distribution Utility Meet (DUM) since 2017.The first edition of DUM in 2017 was hosted by Bangalore Electricity Supply Company (BESCOM) in November 2017 in Bangalore and the second edition was hosted by Tata Power Delhi Distribution Ltd (TPDDL) and Tata Power Mumbai Mumbai in Nov 2018. DUM 2018 was a huge success with participation of 35+ utilities from India. DUM provides a unified voice to influence and enable the DISCOM community to leverage each other's experiences for successful nationwide smart grid roll outs and to bridge the gap between strategy and execution. Leading utilities from USA, Canada and Europe also participated in DUM 2018 and shared their experiences in implementing Smart Grid Technologies. Third edition of DUM 2019 will be held on 07 - 08 November 2019 in New Delhi and will be hosted by BRPL, BYPL, TPDDL and Tata Power Mumbai.
+<br><br>
+
+	</p>
+</div>
+
+<div class="row k_container k_key_themes" style="background: #fff;">
+
+	<div class="k_cheading">
+
+		<h1>Industry  <span class="k_greencolor"> Participation</span> </h1>
+
+		<img src="images/kborder_bottom.png" alt="">
+
+	</div>
+
+	<p class="k_p">Utilities and select Technology Providers have the option to exhibit their products and solutions in the Exhibition Booths which will give a unique experience of interacting with visitors from around the globe and explaining about their offerings.</p>
+
+	<p class="k_p">For Special participation packages, list of features of the booth and other enquiry please write to <a href="mailto:Sneha@indiasmartgrid.org">Sneha@indiasmartgrid.org</a>
+
+    <br/>For Delegate Participation and Registration, please write to us at <a href="mailto:dum@indiasmartgrid.org">dum@indiasmartgrid.org</a></p>
+
+    <div class="k_cheading"><a href="https://register.indiasmartgrid.org/">
+
+	<img src="images/Register_Now_Button.png" style="width:20%; text-align: center;"></a>
+
+	</div>
+
+<!-- 	<div class="col-sm-5 img-part"><img src="images/k_keythemes.jpg" alt=""></div> -->
+
+	
+
+</div>
+
+
+
+
+
+<!-- vineet dum 2017 text end -->
+<?php include "include/footer.php"; ?>

67d366a84e95c26504382f5b84bb09208e24204b
msrmenterprises <connect@msrmenterprises.com>
2026-07-24 16:46:13 -0500

    IWT-582

--- index.php
+++ index.php
@@ -385,7 +385,7 @@ Please contact  : <a href="mailto:Sneha@indiasmartgrid.org">Sneha@indiasmartgrid
 
 	<ul class="k_confirmed_als">
 		<h5><b>THEMATIC SESSION PARTNER</b></h5>
-		<li><a href="https://www.oracle.com/in/utilities/" target="_blank"><img src="https://upload.wikimedia.org/wikipedia/commons/5/50/Oracle_logo.svg" style="border: 1px solid #aeb4b2; height: 100px;" alt="Oracle">Oracle</a></li>
+		<li><a href="https://www.oracle.com/in/utilities/" target="_blank"><img src="images/2026/OracleUtilities.png" style="border: 1px solid #aeb4b2; height: 100px;" alt="Oracle">Oracle</a></li>
 	</ul>
 
 	<br>
@@ -410,7 +410,7 @@ Please contact  : <a href="mailto:Sneha@indiasmartgrid.org">Sneha@indiasmartgrid
 
 	<ul class="k_confirmed_als">
 		<h5><b>GOLD EXHIBITORS</b></h5>
-		<li><a href="https://www.oracle.com/in/utilities/" target="_blank"><img src="https://upload.wikimedia.org/wikipedia/commons/5/50/Oracle_logo.svg" style="border: 1px solid #aeb4b2; height: 100px;" alt="Oracle">Oracle</a></li>
+		<li><a href="https://www.oracle.com/in/utilities/" target="_blank"><img src="images/2026/OracleUtilities.png" style="border: 1px solid #aeb4b2; height: 100px;" alt="Oracle">Oracle</a></li>
 		<li><a href="https://www.indiasmartgrid.org/" target="_blank"><img src="images/2026/ISGF.png" style="height:120px;" alt="ISGF">ISGF</a></li>
 	</ul>
 

c32f6e5def4b87d81fe50b517d642ffbcd2ab8ec
msrmenterprises <connect@msrmenterprises.com>
2026-08-11 21:45:21 -0500

    IWT612

--- agenda.php
+++ agenda.php
@@ -7,21 +7,21 @@
 
 <div class="row dum_container">
 	<div class="k_cheading">
-		<h1><span class="k_greencolor">DUM 2025 AGENDA</span> </h1>
+		<h1><span class="k_greencolor">DUM 2026 AGENDA</span> </h1>
 		<img src="images/kborder_bottom.png" alt="">
 		
 		<br>
 
 		<!-- <iframe src="dumpdf/Dum24.pdf" style="width:90%;height:500px;"></iframe><br><br> -->
-		<!-- <div class="col-sm-12" style="text-align: center;">
-			<p style="font-size:17px; text-align:center;">
-				<b>Agenda of DUM 2025 will be updated closer to the Program on Website. To Participate, please write to Organizers at <br/> <a href="mailto:dum@Indiasmartgrid.org">dum@Indiasmartgrid.org</a>
-			</p>
-		</div> -->
 
-		
 		<div class="col-sm-12" style="text-align: center;">
-			<a href="http://dumindia.in/images/2026/DUM25_agenda.pdf" target="_blank" style="font-size: 28px; font-weight: 800; text-decoration: none;">Download Agenda</a>
+			<p style="font-size:17px; text-align:center;">
+				Agenda will be uploaded closer to the event date. For details, please email us at <a href="mailto:dum@indiasmartgrid.org">dum@indiasmartgrid.org</a>.
+			</p>
+		</div>
+
+		<div class="col-sm-12" style="text-align: center;">
+			<a href="http://dumindia.in/images/2026/DUM25_agenda.pdf" target="_blank" style="font-size: 28px; font-weight: 800; text-decoration: none; color: #aaaaaa; pointer-events: none; cursor: not-allowed;">Download Agenda</a>
 		</div>
 		<br>
 		<br>
